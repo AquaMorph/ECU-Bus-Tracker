07d8b9089371e6856ce71d7a2df16799839cbe62
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-09-10 21:29:44 -0400

    Launched predictions activity based on selected route

--- app/src/main/java/com/aquamorph/ecubustracker/MainActivity.java
+++ app/src/main/java/com/aquamorph/ecubustracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aquamorph.ecubustracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,17 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 
 import com.aquamorph.ecubustracker.Models.Routes;
 import com.aquamorph.ecubustracker.Parsers.RouteList;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 	private String TAG = "MainActivity";
 	public static final String URL = "http://webservices.nextbus.com/service/publicXMLFeed";
 	public static final String UNIVERSITY = "&a=ecu";
@@ -29,8 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getRoutes();
-		Button button = (Button) findViewById(R.id.button);
-		button.setOnClickListener(this);
 
 		recyclerView = (RecyclerView) findViewById(R.id.routeRV);
 		adapter = new RouteListAdapter(getApplicationContext(), routes);
@@ -38,6 +38,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		llm.setOrientation(LinearLayoutManager.VERTICAL);
 		recyclerView.setAdapter(adapter);
 		recyclerView.setLayoutManager(llm);
+		recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this, recyclerView, new ClickListener() {
+			@Override
+			public void onClick(View view, int position) {
+				launchPredictions(routes.get(position).getTitle().substring(0,3));
+			}
+
+			@Override
+			public void onLongClick(View view, int position) {
+				launchPredictions(routes.get(position).getTitle().substring(0,3));
+			}
+		}));
 	}
 
 	@Override
@@ -69,18 +80,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		startActivity(intent);
 	}
 
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-			case R.id.button:
-				Intent intent = new Intent(this, PredictionsActivity.class);
-				Bundle bundle = new Bundle();
-				bundle.putString("route", "803");
-				intent.putExtras(bundle);
-				startActivity(intent);
-				break;
-		}
+	public void launchPredictions(String route) {
+		Intent intent = new Intent(this, PredictionsActivity.class);
+		Bundle bundle = new Bundle();
+		bundle.putString("route", route);
+		intent.putExtras(bundle);
+		startActivity(intent);
 	}
 
 	public void getRoutes() {
@@ -106,5 +111,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				Log.i(TAG, "Routes: " + routes.get(i).getTitle());
 			}
 		}
+	}
+
+	class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+		private GestureDetector gestureDetector;
+		private ClickListener clickListener;
+
+		public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+			this.clickListener = clickListener;
+			gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+				@Override
+				public boolean onSingleTapUp(MotionEvent e) {
+//					return super.onSingleTapUp(e);
+					return true;
+				}
+
+				@Override
+				public void onLongPress(MotionEvent e) {
+					View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+					if(child!=null && clickListener!=null) {
+						clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+					}
+					super.onLongPress(e);
+				}
+			});
+		}
+
+		@Override
+		public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+			View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+			if(child!=null && clickListener!=null && gestureDetector.onTouchEvent(e)) {
+				clickListener.onClick(child, rv.getChildPosition(child));
+			}
+			Log.d(TAG, "onInterceptTouchEvent" + gestureDetector.onTouchEvent(e) + e);
+			return false;
+		}
+
+		@Override
+		public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+			Log.d(TAG, "onTouchEvent" + e);
+
+		}
+
+		@Override
+		public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+		}
+	}
+
+	public static interface ClickListener {
+		public void onClick(View view, int position);
+		public void onLongClick(View view, int position);
 	}
 }
