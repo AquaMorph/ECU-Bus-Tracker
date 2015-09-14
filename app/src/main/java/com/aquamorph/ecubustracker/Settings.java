bf4a8ee8227e06687890f733b3f2b15b1d9a4cf4
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-09-14 11:06:05 -0400

    Added back arrow to settings

--- app/src/main/java/com/aquamorph/ecubustracker/Settings.java
+++ app/src/main/java/com/aquamorph/ecubustracker/Settings.java
@@ -3,6 +3,7 @@ package com.aquamorph.ecubustracker;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class Settings extends AppCompatActivity {
 	@Override
@@ -12,5 +13,20 @@ public class Settings extends AppCompatActivity {
 
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		toolbar.setTitle("Settings");
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				this.finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
